d4bb2782ada8a11d37148fd0d7ab5ac53aa26a87
Neil <nokhand@gmail.com>
2015-10-25 17:57:02 -0500

    maybe fixed

--- code.py
+++ code.py
@@ -36,7 +36,7 @@ def getStuffBetween(tag, text):
 
 
 data = ""
-with open('content.txt', 'r') as text:
+with open('content2.txt', 'r') as text:
     data = text.read()
 
 lines = data.splitlines()
@@ -44,24 +44,36 @@ lines = data.splitlines()
 items, numbers = getStuffBetween("item", lines)
 
 fixeditems = []
-count =0 
 for item in items:
+    # get the article url
     link = re.search("<link>(.*?)</link>", item)
     link = link.group(1)
+    # scrape the content (curl | tidy | grep word_count
     fullcontent = getFullContent(link)
+
+    # and now we just need to remove the paragraph attributes because who cares
     newcontent = fullcontent.splitlines()
     newcontent = [re.sub(r"<p.*?>", r"<p>", p) for p in newcontent]
     newcontent = ' '.join(newcontent)
-    #newcontent = "------ ITEM "+str(count)+" CONTENT ------"
-    #count +=1
-    fixeditem = re.sub(r"<content:encoded><!\[CDATA\[.*?\]\]>", r"<content:encoded><![CDATA["+newcontent+"]]>", item) # don't need to escape the sub-side
+    # search
+    head = r"<description><!\[CDATA\[<p class=\"descender\">"
+    tail = r".*?\]\]"
+    search = head + tail
+    # replace; don't need to escape for substitution
+    head2 = r"<description><![CDATA[<p class=\"descender\">"
+    tail2 = r"]]"
+    replace = head2 + newcontent + tail2
+
+    fixeditem = re.sub(search, replace, item)
+
+    # and just plug it back in
+#    fixeditem = re.sub(r"<content:encoded><!\[CDATA\[.*?\]\]>", r"<content:encoded><![CDATA["+newcontent+"]]>", item) # don't need to escape the sub-side
     fixeditems.append(fixeditem)
 
 #copy from 0 to start
 #add new item
 #copy from start+1 to next start
 #so on
-
 newlines = []
 lastend = -1
 for index, numberset in enumerate(numbers):
@@ -69,7 +81,6 @@ for index, numberset in enumerate(numbers):
     a = lines[lastend+1:start]
     a.append(fixeditems[index])
     newlines.extend(a)
-    #newlines += lines[laststart:start-1].append(fixeditems[index])
     lastend=end
 _, finend = numbers[len(numbers)-1]
 newlines.extend(lines[finend+1:]) # grabs all the shit after the final item
