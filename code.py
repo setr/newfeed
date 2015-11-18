b6a5cd98484dbe41767bf94144d540836eea9b60
Neil <nokhand@gmail.com>
2015-11-18 13:23:25 -0600

    moved the content into <description>, made downloads multiprocessed

--- code.py
+++ code.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 import re
 import subprocess
+import multiprocessing
+
 
 def getFullContent(link):
+    """gets the actual content of the article"""
     curl = "curl -L " + link
-    # note: tidy -xml can be used to format the final output
     tidy = "tidy --force-output yes --vertical-space no -utf8 -w 100000 2>/dev/null"
     grep = "grep word_count"
     try:
@@ -14,66 +16,92 @@ def getFullContent(link):
     return output
 
 def getStuffBetween(tag, text):
+    """ finds tag data, and notes the lines they appeared on
+         assumes the html was passed through tidy first,
+         as that means we can assume its line-delimited"""
     items = []
     item = ""
     keep = False
     linenumbers = [] # [(start, end)]
     start = 0
     for num, line in enumerate(text):
-        if "<"+tag+">" in line:  # if we open, start keeping the lines
+        if "<"+tag+">" in line: 
             keep = True
             start = num
-        elif "</"+tag+">" in line:  # if we close, we be done
+        elif "</"+tag+">" in line:
             item += line
             keep = False
             items.append(item)
             linenumbers.append((start,num))
-            item = ""
 
+            item = ""
         if keep:
             item += line
 
     return items, linenumbers
 
+def fixItems(items):
+    """Replaces the item's description with the full content 
+        of the article it links to; multiprocessed """
+    fixeditems = multiprocessing.Manager().list()
+    processes = []
+    index = 0
+    for item in items:
+        def work(item, fixeditems, index):
+            # get the link to the full article
+            item = items[0]
+            link = re.search("<link>(.*?)</link>", item)
+            link = link.group(1)
+            # gets the content from that article
+            fullcontent = getFullContent(link)
+            newcontent = fullcontent
+            #newcontent = fullcontent.splitlines()
+            #newcontent = [re.sub(r"<p.*?>", r"<p>", p) for p in newcontent]
+            #newcontent = ' '.join(newcontent)
+            #newcontent = "------ ITEM "+str(count)+" CONTENT ------"
+            #count +=1
+            #fixeditem = re.sub(r"<content:encoded><!\[CDATA\[.*?\]\]>", r"<content:encoded><![CDATA["+newcontent+"]]>", item) # don't need to escape the sub-side
+
+            #now we plug in the content into the description
+            fixeditem = re.sub(r"<!\[CDATA\[.*?\]\]>",
+                               r"<![CDATA["+newcontent+"]]>",
+                               item)
+            # and get rid of the original content:encoded, because we don't need it
+            fixeditem = re.sub(r"<content:encoded>.*?</content:encoded>", 
+                               "", 
+                               fixeditem,
+                               flags=re.S) # sets single-line mode, 
+                                    # so . matches EVERY character, including \n
+            fixeditems.append((index, fixeditem))
+        p = multiprocessing.Process(target=work, 
+                                    args=(item, 
+                                       fixeditems, 
+                                       index))
+        index += 1
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+    fixeditems = sorted(fixeditems)
+    fixeditems = [item[1] for item in fixeditems]
+    return fixeditems
+
 
 data = ""
-with open('content2.txt', 'r') as text:
+with open('content.txt', 'r') as text:
     data = text.read()
 
 lines = data.splitlines()
 
 items, numbers = getStuffBetween("item", lines)
 
-fixeditems = []
-for item in items:
-    # get the article url
-    link = re.search("<link>(.*?)</link>", item)
-    link = link.group(1)
-    # scrape the content (curl | tidy | grep word_count
-    fullcontent = getFullContent(link)
-
-    # and now we just need to remove the paragraph attributes because who cares
-    newcontent = fullcontent.splitlines()
-    newcontent = [re.sub(r"<p.*?>", r"<p>", line) for line in newcontent]
-    newcontent = ' '.join(newcontent)
-
-    # and now we plug the content into the item
-    # search
-    head = r"<description><!\[CDATA\[<p class=\"descender\">"
-    tail = r".*?\]\]"
-    search = head + tail
-    # replace; don't need to escape for substitution
-    head2 = r"<description><![CDATA[<p class=\"descender\">"
-    tail2 = r"]]"
-    replace = head2 + newcontent + tail2
-
-    fixeditem = re.sub(search, replace, item)
-    fixeditems.append(fixeditem)
-
+fixeditems = fixItems(items)
 #copy from 0 to start
 #add new item
 #copy from start+1 to next start
 #so on
+
 newlines = []
 lastend = -1
 for index, numberset in enumerate(numbers):
@@ -81,10 +109,10 @@ for index, numberset in enumerate(numbers):
     a = lines[lastend+1:start]
     a.append(fixeditems[index])
     newlines.extend(a)
+    #newlines += lines[laststart:start-1].append(fixeditems[index])
     lastend=end
 _, finend = numbers[len(numbers)-1]
 newlines.extend(lines[finend+1:]) # grabs all the shit after the final item
 
 done = "\n\n ".join(newlines)
 print done
-    
