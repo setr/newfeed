5f9bbf8d03c21e92cd6c79569b5c73aca8a9de3f
Neil <nokhand@gmail.com>
2015-11-22 17:38:41 -0600

    Wiped out the crud (txt files generated for testing)

--- code.py
+++ code.py
@@ -2,7 +2,33 @@
 import re
 import subprocess
 import multiprocessing
+from readability.readability import Document
+import urllib
 
+
+def getYorkerContent(link):
+    html = urllib.urlopen(link).read()
+    extraNegs = "sendlater-container"
+    content = Document(html, url=link, negative_keywords=extraNegs).summary()
+    # rips out the newsletter portion, because I couldn't get readability neg keywords to do the job
+    content = re.sub(r'<section id="newsletter-signup">.*?</section>',
+                        "",
+                        content,
+                        flags=re.S) # sets single-line mode
+    # added in by readability, because it assumes it's work is the end product
+    # rss probably doesn't like it though, so burn it
+    content = re.sub(r'<html><body>', "", content)
+    content = re.sub(r'</body></html>', "", content)
+    return content
+
+    # getContent = "python -m readability.readability -u "+link+" -n " + extraNegs
+    # sedFix = "sed -E 's/<section id=\"newsletter-signup\">.*?</section>//g'"
+    # try:
+    #     output = subprocess.check_output(getContent + " | " + sedFix, shell=True)
+    # except subprocess.CalledProcessError as e:
+    #     output = str(e.output)
+    # return output
+getContent = getYorkerContent
 
 def getFullContent(link):
     """gets the actual content of the article"""
@@ -40,6 +66,27 @@ def getStuffBetween(tag, text):
 
     return items, linenumbers
 
+# does our parsing
+def work(item, fixeditems, index):
+    # get the link to the full article
+    link = re.search("<link>(.*?)</link>", item)
+    link = link.group(1)
+    # gets the content from that article
+    fullcontent = getContent(link)
+    newcontent = fullcontent.encode("utf-8")
+
+    #now we plug in the content into the description
+    fixeditem = re.sub("<!\[CDATA\[.*?\]\]>",
+                        "<![CDATA["+newcontent+"]]>",
+                        item)
+    # and get rid of the original content:encoded, because we don't need it
+    fixeditem = re.sub("<content:encoded>.*?</content:encoded>", 
+                        "", 
+                        fixeditem,
+                        flags=re.S) # sets single-line mode, 
+                            # so . matches EVERY character, including \n
+    fixeditems.append((index, fixeditem))
+
 def fixItems(items):
     """Replaces the item's description with the full content 
         of the article it links to; multiprocessed """
@@ -47,40 +94,16 @@ def fixItems(items):
     processes = []
     index = 0
     for item in items:
-        def work(item, fixeditems, index):
-            # get the link to the full article
-            item = items[0]
-            link = re.search("<link>(.*?)</link>", item)
-            link = link.group(1)
-            # gets the content from that article
-            fullcontent = getFullContent(link)
-            newcontent = fullcontent
-            #newcontent = fullcontent.splitlines()
-            #newcontent = [re.sub(r"<p.*?>", r"<p>", p) for p in newcontent]
-            #newcontent = ' '.join(newcontent)
-            #newcontent = "------ ITEM "+str(count)+" CONTENT ------"
-            #count +=1
-            #fixeditem = re.sub(r"<content:encoded><!\[CDATA\[.*?\]\]>", r"<content:encoded><![CDATA["+newcontent+"]]>", item) # don't need to escape the sub-side
-
-            #now we plug in the content into the description
-            fixeditem = re.sub(r"<!\[CDATA\[.*?\]\]>",
-                               r"<![CDATA["+newcontent+"]]>",
-                               item)
-            # and get rid of the original content:encoded, because we don't need it
-            fixeditem = re.sub(r"<content:encoded>.*?</content:encoded>", 
-                               "", 
-                               fixeditem,
-                               flags=re.S) # sets single-line mode, 
-                                    # so . matches EVERY character, including \n
-            fixeditems.append((index, fixeditem))
-        p = multiprocessing.Process(target=work, 
-                                    args=(item, 
-                                       fixeditems, 
+        p = multiprocessing.Process(target=work,
+                                    args=(item,
+                                       fixeditems,
                                        index))
         index += 1
         processes.append(p)
-        p.start()
+        item = ""
 
+    for p in processes:
+        p.start()
     for p in processes:
         p.join()
     fixeditems = sorted(fixeditems)
@@ -115,4 +138,8 @@ _, finend = numbers[len(numbers)-1]
 newlines.extend(lines[finend+1:]) # grabs all the shit after the final item
 
 done = "\n\n ".join(newlines)
+
+done = re.sub(r"<atom:link.*?/>", # gets rid of the newyorker pubsubhubbub shit
+              "",
+              done)
 print done
--- updateyorker.sh
+++ updateyorker.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export PYTHONIOENCODING=UTF-8
 curl http://www.newyorker.com/rss > content.txt
 ./code.py > finalyorker.rss
 git commit finalyorker.rss -m "auto-update finalyorker.rss"
